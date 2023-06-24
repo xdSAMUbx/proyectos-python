1ef8ec3abadcbf49d39d8425db667bd50cda33a4
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-23 19:24:40 -0500

    calculadora

---
+++ principiante/calculadora_basica.py
@@ -0,0 +1,27 @@
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número:  "))
+
+print("¿Qué desea hacer?")
+print("1) Sumar")
+print("2) Restar")
+print("3) Multiplicar")
+print("4) Dividir")
+
+decision = int(input("Ingrese la opción que desea realizar (números): "))
+
+if decision == 1:
+    resultado = numero1 + numero2
+    print(f"El resultado de su suma es: {resultado}")
+elif decision == 2:
+    resultado = numero1 - numero2
+    print(f"El resultado de su suma es: {resultado}")
+elif decision == 3:
+    resultado = numero1 * numero2
+    print(f"El resultado de su suma es: {resultado}")
+elif decision == 4:
+    resultado = round(numero1 / numero2, 3)
+    print(f"El resultado de su suma es: {resultado}")
+else: 
+    print("No ingreso ninguna opción valida, intente de neuvo")
+
+    
