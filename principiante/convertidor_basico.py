a237ecc0b530e6c2a932086d376b57511c44f7fd
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-30 20:00:41 -0500

    convertidorbasico

--- principiante/convertidor_basico.py
+++ principiante/convertidor_basico.py
@@ -1,15 +1,6 @@
 #convertidor de unidades
 
-medida = float(input("Ingrese la medida que desea convertir: "))
-
-def inicial ():
-    print("De las siguientes opciones: ")
-    print("1) Metros: ")
-    print("2) Pulgadas: ")
-    print("3) Pies: ")
-    print("4) Millas: ")
-    opciones1 = int(input("Digite la medida de la cual viene: "))
-    return opciones1
+medida = float(input("Ingrese la medida que desea convertir (en Metros): "))
 
 def final ():
     print("De las siguientes opciones: ")
@@ -21,27 +12,26 @@ def final ():
     return opciones2
 
 def convertidor (medida):
+    opcion_final = final()
     while medida > 0:
-        opcion_inicial = inicial()
-        opcion_final = final()
-        while opcion_inicial == 1:
-            if opcion_final == 2:
+        if opcion_final == 1:
+            print("No se puede convertir de metros a metros")
+            break
+        elif opcion_final == 2:
                 conversion = medida * 39.37
                 print(f"1 Metro equivale a 39.37 Pulgadas, por lo tanto {medida} metros equivalen a {conversion} pulgadas")
                 break
-            elif opcion_final == 3:
-                conversion = medida * 39.37
-                print(f"1 Metro equivale a 39.37 Pulgadas, por lo tanto {medida} metros equivalen a {conversion} pulgadas")
+        elif opcion_final == 3:
+                conversion = medida * 3.28
+                print(f"1 Metro equivale a 3.28 pies , por lo tanto {medida} metros equivalen a {conversion} pies")
                 break
-            elif opcion_final == 4:
-                conversion = medida * 39.37
-                print(f"1 Metro equivale a 39.37 Pulgadas, por lo tanto {medida} metros equivalen a {conversion} pulgadas")
+        elif opcion_final == 4:
+                conversion = medida * 0.000621
+                print(f"1 Metro equivale a 0.000621 Millas, por lo tanto {medida} metros equivalen a {conversion} millas")
                 break
-            else:
+        else:
                 print("No sabemos a que medida desea convertir")
-                
-        
-    print("Sin medida no podemos continuar, intentelo de nuevo")
+                break
 
 convertidor(medida)
 
